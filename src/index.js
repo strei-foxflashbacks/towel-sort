d1b2cf0613247d7221f8212440210343981c230a
ssekridov@gmail.com <ssekridov@gmail.com>
2022-08-06 00:02:59 +0300

    feat task solved

--- src/index.js
+++ src/index.js
@@ -2,5 +2,27 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === undefined) {
+    return []
+  }
+  if (matrix.length === 0) {
+    return []
+  }
+  else {
+    matrix.reduce((accumulator, currentValue, index) => {
+      const sortedVal = currentValue.sort(function (a, b) {
+        if (index % 2 === 0) {
+          return a - b
+        }
+        else {
+          return b - a
+        }
+      })
+      sortedVal.map(function (element) {
+        accumulator.push(element)
+      })
+      return accumulator
+    }, [])
+    return matrix.join().split(',').map(Number)
+  }
 }
